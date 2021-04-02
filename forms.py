1ad9ff34e2ec0ad06cbc221dc630695f55ac8457
bladluka <78096877+bladluka@users.noreply.github.com>
2021-04-02 13:47:09 +0200

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, FloatField
-from wtforms.validators import DataRequired
-
-class SpendingsForm(FlaskForm):
-    description = StringField('Wydatek', validators=[DataRequired()])
-    category = StringField('Kategoria', validators=[DataRequired()])
-    value = FloatField('Wartość', validators=[DataRequired()])
-    done = BooleanField('Potwierdź', validators=[DataRequired()])
